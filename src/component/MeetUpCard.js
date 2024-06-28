b56794e279b56ca925cd069772d9babb8a11e972
Soo Jung <comme746@naver.com>
2024-06-28 18:18:40 +0900

    test

--- src/component/MeetUpCard.js
+++ src/component/MeetUpCard.js
@@ -22,12 +22,12 @@ const MeetUpCard = ({ meetUp }) => {
             </div>
             <div className='contents'>
                 <div className='title'>{meetUp.title}</div>
-                {/* <div className='schedule green'>
+                <div className='schedule green'>
                     <FontAwesomeIcon icon={faLocationDot} style={{ color: "#28A745", }} />{" "}
                     {meetUp.location === "online" ? (<span>온라인 </span>) : (<span>{meetUp?.location.split(' ')[1]} </span>)}
                     ·{" "}
                     {
-                        meetUp.date.date === format(new Date(), 'yyyy.MM.dd') ?
+                        meetUp.date.date === format(new Date(), 'yyyy/MM/dd') ?
                             (<span>오늘</span>)
                             :
                             (<span>
@@ -35,7 +35,7 @@ const MeetUpCard = ({ meetUp }) => {
                                 {format(parse(meetUp.date.time, 'HH:mm:ss', new Date()), 'a h:mm', { locale: ko })}
                             </span>)
                     }
-                </div> */}
+                </div>
                 <div className='small-text'>{meetUp.organizer.nickName} · 선착순 {meetUp.currentParticipants}/{meetUp.maxParticipants}</div>
             </div>
         </div>
