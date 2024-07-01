abed1ffd08757fb849ac498694eb5e84ed6f7ee3
Soo Jung <comme746@naver.com>
2024-07-01 17:18:26 +0900

    test

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const REACT_APP_BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY
 // const socket = io(REACT_APP_BACKEND_PROXY);
 
 const socket = io(REACT_APP_BACKEND_PROXY, {
+    path: '/socket.io',
     transports: ['websocket', 'polling'],
     secure: true,
     reconnection: true,
