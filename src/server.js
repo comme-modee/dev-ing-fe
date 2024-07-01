3fa08178b54198b70c249d5a099b5f5bb3766b5c
Soo Jung <comme746@naver.com>
2024-07-01 17:09:09 +0900

    test

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 import { io } from "socket.io-client";
 const REACT_APP_BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY
 
-const socket = io(REACT_APP_BACKEND_PROXY);
+// const socket = io(REACT_APP_BACKEND_PROXY);
+
+const socket = io(REACT_APP_BACKEND_PROXY, {
+    transports: ['websocket', 'polling'],
+    secure: true,
+    reconnection: true,
+});
 
 export default socket;
