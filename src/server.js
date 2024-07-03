38c351d30d786762e76c56eda76fd04776adbaac
Soo Jung <comme746@naver.com>
2024-07-03 18:21:04 +0900

    update

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 const REACT_APP_BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY
 
 // const socket = io(REACT_APP_BACKEND_PROXY);
-const socket = io(`https://port-0-dev-ing-server-1272llwyghb5h.sel5.cloudtype.app`);
+const socket = io(`http://localhost:5001`);
 
 // const socket = io(REACT_APP_BACKEND_PROXY, {
 //     path: '/socket.io',
