4684e57ba1e36c16acde63412a81e1f793a1274b
Soo Jung <comme746@naver.com>
2024-07-03 18:23:33 +0900

    update

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
 const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 
 const api = axios.create({
-  baseURL: `${LOCAL_BACKEND}/api`,
+  baseURL: `https://port-0-dev-ing-server-1272llwyghb5h.sel5.cloudtype.app/api`,
 //   baseURL: `${BACKEND_PROXY}/api`,
   headers: {
     "Content-Type": "application/json",
