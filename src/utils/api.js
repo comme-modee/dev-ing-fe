2faa687f7df8eacec0f0bb17f264c427d710813b
Soo Jung <comme746@naver.com>
2024-07-04 14:00:40 +0900

    update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 // 상황따라 주소 다름
-// const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
+const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
 // const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
 // const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 
 const api = axios.create({
-  baseURL: `https://port-0-dev-ing-server-1272llwyghb5h.sel5.cloudtype.app/api`,
-//   baseURL: `${LOCAL_BACKEND}/api`,
+//   baseURL: `https://port-0-dev-ing-server-1272llwyghb5h.sel5.cloudtype.app/api`,
+  baseURL: `${LOCAL_BACKEND}/api`,
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
